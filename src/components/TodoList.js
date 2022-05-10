e9d9929d1009c5e79d441eb0c17299a165af5bbd
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-10 23:42:47 +0300

    new commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,12 +8,17 @@ function TodoList() {
     if(!todo.text || /^\$/.test(todo.text)) {
       return
     }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    
   }
 
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }

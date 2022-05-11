159a45721d6f3d0c0a196d130ed9eb31299fdafc
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-11 17:46:46 +0300

    new commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,15 +4,20 @@ import TodoList from './TodoList';
 
 function Todo() {
   const [edit, setEdit] = useState({
-      id: null,
-      value: ''
-  })
+    id: null,
+    value: '',
+  });
 
-  return TodoList.map((todo, index) => (
-      <div className={todo.isComplete ? 'todo-tow complete' : 'todo-row'} key={index}>
-          <div key={todo.id} onClick={() => completeTodo(todo.id)}>{todo.text}</div>
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
       </div>
-  ))
+    </div>
+  ));
 }
 
 export default Todo;
